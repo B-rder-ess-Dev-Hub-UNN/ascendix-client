07d618159f553bc15a244464c3f92c259d9db765
Otuja <johnpraiseotuka@gmail.com>
2025-08-16 16:29:02 +0100

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PrimaryLayout from './layouts/PrimaryLayout';
 import Profile from './pages/Profile';
 import SettingPage from './pages/SettingPage';
 import HelpCenter from './pages/HelpCenter';
+import HomePage from './pages/HomePage';
 
 
 const App = () => {
@@ -24,6 +25,7 @@ const App = () => {
         </Route>
 
         <Route element={<PrimaryLayout />}>
+          <Route path="/home" element={<HomePage />} />
           <Route path="/email" element={<EmailVerified />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/settings" element={<SettingPage />} />
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,78 @@
+import course from "../assets/images/Frame 8.png";
+import library from "../assets/images/Frame 9.png";
+import quest from "../assets/images/Frame 10.png";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import QuestCard from "../components/QuestCard";
+import LeaderCard from "../components/LeaderCard";
+
+const HomePage = () => {
+  return (
+    <section className="bg-[#1D1751] min-h-screen flex flex-col text-white p-5 md:p-20 space-y-10">
+      {/* Hero section  */}
+      <div className=" flex flex-col items-center text-center justify-center space-y-4 md:px-50">
+        <h1 className="font-extrabold text-3xl md:text-6xl">
+          <span className="text-[#B6E63A]">Earn</span> money in Web3: Learn
+          while you earn.
+        </h1>
+        <p className="font-semibold text-xl md:text-3xl">
+          "Learn, Earn, Conquer quests!"
+        </p>
+        <Link
+          to=""
+          className="border border-[#B6E63A] py-3 px-8 rounded-2xl font-medium"
+        >
+          Start Earning Now
+        </Link>
+      </div>
+
+      {/* quest section  */}
+      <div className="flex flex-col space-y-5">
+        <div className="flex items-center space-x-8">
+          <p className="font-medium text-xl">Top Quest Picked</p>
+          <Link to="#" className="flex items-center space-x-2">
+            <span className="font-medium text-[#B6E63A]">View all</span>
+            <FaArrowRight className="" />
+          </Link>
+        </div>
+
+        <QuestCard />
+      </div>
+
+      {/* Leaderboard section */}
+      <div className="flex flex-col space-y-5">
+        <div className="flex items-center space-x-8">
+          <p className="font-medium text-xl">Leaderboard</p>
+          <Link to="#" className="flex items-center space-x-2">
+            <span className="font-medium text-[#B6E63A]">View all</span>
+            <FaArrowRight className="" />
+          </Link>
+        </div>
+
+        <LeaderCard />
+      </div>
+
+      {/* earn section  */}
+      <div className="space-y-5">
+        <div>
+          <h2 className="font-semibold text-2xl">Have a minute?</h2>
+          <p className="font-semibold text-2xl text-[#B6E63A]">Earn now!</p>
+        </div>
+
+        <div className="flex flex-col space-y-5 md:flex-row md:space-x-5 md:space-y-0">
+          <Link to="#">
+            <img src={course} alt="" />
+          </Link>
+          <Link to="#">
+            <img src={library} alt="" />
+          </Link>
+          <Link to="#">
+            <img src={quest} alt="" />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
